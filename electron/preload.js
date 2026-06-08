27376b7ef7a2ca4fe7f34e5dad062b00b614d4be
TerryBlackhoodWoo <leftdeadman@gmail.com>
2026-06-08 17:52:19 +0900

    feat: add openFile, openFolder IPC bridge

--- electron/preload.js
+++ electron/preload.js
@@ -8,7 +8,13 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("voxscript", {
   // 로컬 파일 선택 다이얼로그
   selectFile: () => ipcRenderer.invoke("select-file"),
-  
+
   // API 포트 가져오기
   getApiPort: () => ipcRenderer.invoke("get-api-port"),
+
+  // 파일 열기
+  openFile: (path) => ipcRenderer.invoke("open-file", path),
+
+  // 폴더 열기
+  openFolder: (path) => ipcRenderer.invoke("open-folder", path),
 });
